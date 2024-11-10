64e594e87fae132decb27ad2424f3cf11dfb9bd5
Minsu4302 <gkdltpa1@naver.com>
2024-11-10 14:27:01 +0900

    Add files via upload

---
+++ Assets/Script/ItemSnow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSnow : MonoBehaviour
+{
+    public float rotateSpeed;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "player")
+        {
+            PlayerBall player = other.GetComponent<PlayerBall>();
+            player.itemCount++;
+            gameObject.SetActive(false);
+        }
+    }
+}
